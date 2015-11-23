3b7e156d3fb65e46a65e8eb9c0f5aeefc520a880
Alejandrogf <alejandrogarciaf@gmail.com>
2015-11-23 14:25:39 +0100

    AutenticacionPersonalizada

---
+++ AutenticacionPersonalizada/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AutenticacionPersonalizada.Utilidades;
+
+namespace AutenticacionPersonalizada.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            //Para recuperar valores definidos en webconfig como keys
+            var clave = ConfigurationManager.AppSettings["ClaveCifrado"];
+
+            //Se pasan dos argumentos: la pwd que pone el usuario y 
+            //la key (que es fija y definida en webconfig, por ej)
+            var cifrado = SeguridadUtilidades.Cifrar("Hola Don Pepito", clave);
+
+            var datosCifrados = Convert.FromBase64String(cifrado);
+
+            //Se pasan dos argumentos: el contenido cifrado recuperado de la db y
+            //la key (que es fija y definida en webconfig, por ej)
+            var descifrado = SeguridadUtilidades.Descifrar(datosCifrados, clave);
+            return View();
+        }
+    }
+}
